f6b3c38f0e56f97e0b6cc8272053614e84ef93ff
Veronicandemo <ndemoveronica@gmail.com>
2023-10-31 13:41:56 +0300

    Responsiveness to pages

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -77,7 +77,7 @@ export default function SignUp() {
         <div className="h-40-r w-40-r bg-gradient-to-r from-green-400 to-blue-500 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
         <div className="h-35-r w-35-r bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-full absolute top-96 -left-20 transform rotate-180 animate-pulse"></div>
 
-        <div className="container sm:h-[100%] h-[80%] md:w-[55%] bg-white bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
+        <div className="container  h-[80%] md:w-[55%] bg-white bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
           <form
             className="h-full  flex flex-col justify-evenly items-center"
             ref={form}
